c412d581bfe4030b4e7247d79c055baab9820ea5
Hyunjun Lee <leehyunjune11@gmail.com>
2024-02-20 19:38:30 +0900

    plus

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = '7193a00c8b994a8da934dcf151157b84';
 let news =[];
 const getlatestNews = async() => {
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=kr&apiKey=${API_KEY}`);
+    const url = new URL(`https://praticenews.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
 
     const response = await fetch(url);
     const data = await response.json();
